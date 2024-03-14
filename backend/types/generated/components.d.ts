25ecf5075f6b41e5f15a70c3a62cf330efe10844
Septian Maulana <septiandev028@gmail.com>
2024-03-14 15:10:50 +0700

    feat: create hero section as layout components in strapi

--- backend/types/generated/components.d.ts
+++ backend/types/generated/components.d.ts
@@ -1,5 +1,22 @@
 import type { Schema, Attribute } from '@strapi/strapi';
 
+export interface LayoutHeroSection extends Schema.Component {
+  collectionName: 'components_layout_hero_sections';
+  info: {
+    displayName: 'Hero Section';
+    icon: 'layout';
+  };
+  attributes: {
+    heading: Attribute.String;
+    subHeading: Attribute.Text;
+    image: Attribute.Media;
+  };
+}
+
 declare module '@strapi/types' {
-  export module Shared {}
+  export module Shared {
+    export interface Components {
+      'layout.hero-section': LayoutHeroSection;
+    }
+  }
 }
