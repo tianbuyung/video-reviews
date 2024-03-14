a381f0e387c87e44bbfcfa7151003f26c9721d8f
Septian Maulana <septiandev028@gmail.com>
2024-03-14 15:38:43 +0700

    chore: update fetch request in home page using qs library

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,8 +1,29 @@
-async function getStrapiData(url: string) {
+import qs from "qs";
+
+const homePageQuery = qs.stringify({
+  populate: {
+    blocks: {
+      populate: {
+        image: {
+          fields: ["url", "alternativeText"],
+        },
+        link: {
+          populate: true,
+        },
+      },
+    },
+  },
+});
+
+async function getStrapiData(path: string) {
   const baseUrl = "http://localhost:1337";
+  const url = new URL(path, baseUrl);
+  url.search = homePageQuery;
+
   try {
-    const response = await fetch(baseUrl + url);
+    const response = await fetch(url.href);
     const data = await response.json();
+
     return data;
   } catch (error) {
     console.error(error);
