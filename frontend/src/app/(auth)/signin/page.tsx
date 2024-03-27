23228e720d6feff4fe42fa8f35b2a839964143b0
Septian Maulana <septiandev028@gmail.com>
2024-03-27 19:22:31 +0700

    feat: create group routes in nextjs for authentication

---
+++ frontend/src/app/(auth)/signin/page.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function SignInRoute() {
+  return <div>Sign In Route</div>;
+}
---
+++ frontend/src/app/(auth)/signup/page.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function SignUpRoute() {
+  return <div>Sign Up Route</div>;
+}
