e9ba116f6ab34ea02e8d457ca35062042fa0d4d8
Septian Maulana <septiandev028@gmail.com>
2024-05-01 10:41:08 +0700

    feat: finish the middleware logic

--- backend/src/middlewares/user-find-many.js
+++ backend/src/middlewares/user-find-many.js
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 /**
  * `user-find-many` middleware
@@ -7,7 +7,19 @@
 module.exports = (config, { strapi }) => {
   // Add your own logic here.
   return async (ctx, next) => {
-    strapi.log.info('In user-find-many middleware.');
+    strapi.log.info("In user-find-many-owner middleware.");
+
+    const currentUserId = ctx.state?.user?.id;
+
+    if (!currentUserId) {
+      strapi.log.error("You are not authenticated.");
+      return ctx.badRequest("You are not authenticated.");
+    }
+
+    ctx.query = {
+      ...ctx.query,
+      filters: { ...ctx.query.filters, id: currentUserId },
+    };
 
     await next();
   };
