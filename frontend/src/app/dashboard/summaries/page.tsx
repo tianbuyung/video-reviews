d23eb3fb0080e57f40c37bfaf6925730f5370d83
Septian Maulana <septiandev028@gmail.com>
2024-05-04 18:48:21 +0700

    feat: create search as custom component
    build: install use-debounce library to support handle search function
    
    feat: integrate into summaries page

--- frontend/src/app/dashboard/summaries/page.tsx
+++ frontend/src/app/dashboard/summaries/page.tsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 
 import { getSummaries } from "@/data/loaders";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Search } from "@/components/customs/Search";
 
 interface LinkCardProps {
   id: string;
@@ -33,6 +34,7 @@ export default async function SummariesRoute() {
   if (!data) return null;
   return (
     <div className="grid grid-cols-1 gap-4 p-4">
+      <Search />
       <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
         {data.map((item: LinkCardProps) => (
           <LinkCard key={item.id} {...item} />
